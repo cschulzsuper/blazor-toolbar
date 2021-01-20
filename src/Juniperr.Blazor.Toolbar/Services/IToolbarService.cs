1dfb8f35c612fcb05a5a91701028069b11f53f33
ChristianJNPRR <christian@juniperr.de>
2021-01-20 16:43:12 +0100

    Added already written toolbar components from other project and defined IToolbarService

---
+++ src/Juniperr.Blazor.Toolbar/Services/IToolbarService.cs
@@ -0,0 +1,13 @@
+﻿namespace Juniperr.Blazor.Toolbar.Services
+{
+    public interface IToolbarService
+    {
+        public void Reset();
+
+        public void Set<T>()
+            where T : ToolbarComponentBase;
+
+        public void Set<T>(int group, int position)
+            where T : ToolbarComponentBase;
+    }
+}
---
+++ src/Juniperr.Blazor.Toolbar/_ToolbarComponentBase.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Juniperr.Blazor.Toolbar
+{
+    public abstract class ToolbarComponentBase : ComponentBase
+    {
+    }
+}
