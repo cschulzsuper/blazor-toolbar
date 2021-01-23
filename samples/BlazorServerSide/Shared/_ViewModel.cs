8fe4544b209cef5615df1004e5d192bc0fb46c43
ChristianJNPRR <christian@juniperr.de>
2021-01-23 13:02:15 +0100

    Implemented logic for blazor serverside and webassembly

---
+++ samples/BlazorServerSide/Shared/_ViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace BlazorServerSide.Shared
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private int _counterValue;
+        public int CounterValue
+        {
+            get => _counterValue;
+            set
+            {
+                _counterValue = value; 
+                OnPropertyChanged(nameof(CounterValue));
+            }
+        }
+
+        public void IncrementCounter()
+            => CounterValue++;
+        public void DecrementCounter()
+            => CounterValue--;
+
+    }
+}
